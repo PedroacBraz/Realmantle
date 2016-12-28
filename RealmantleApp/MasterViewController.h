f564b04cc4036cbdb8f4f0f1054c2b9954867b49
Mac Mini Beta <macminibeta@Macs-Mac-mini.local>
2016-12-28 11:24:09 -0200

    Initial Commit

---
+++ RealmantleApp/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  RealmantleApp
+//
+//  Created by Mac Mini Beta on 28/12/16.
+//  Copyright © 2016 ZupBraz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
